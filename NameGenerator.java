adc2ba5423215e3f56f308c73389156a0e3193a6
Alejandro Aller <Alejandroallerdiez@gmail.com>
2014-04-07 09:41:03 +0200

    Cambiado variable estaticas por variables de clase

--- NameGenerator.java
+++ NameGenerator.java
@@ -4,7 +4,10 @@ public class NameGenerator
     private String apellidos;
     private String apellidoMadre;
     private String ciudadDondeNacistes;
-
+    private static final int COMIENZO = 0;
+    private static final int PRIMERA_LETRA = 1;
+    private static final int FINAL = 3;
+    private static final int APELLIDO_FINAL = 2;
     public NameGenerator()
     {   
     }
@@ -15,10 +18,10 @@ public class NameGenerator
         this.apellidos = apellidos;
         this.apellidoMadre = apellidoMadre;
         this.ciudadDondeNacistes = ciudadDondeNacistes; 
-        String nombreStarWars = apellidos.substring(0,1).toUpperCase() +
-        apellidos.substring(1,3).toLowerCase() + nombre.substring(0,3).toLowerCase();
-        String apellidosStarWars = apellidoMadre.substring(0,1).toUpperCase()
-        + apellidoMadre.substring(1,2).toLowerCase() +  ciudadDondeNacistes.substring(0,3).toLowerCase();
+        String nombreStarWars = apellidos.substring(COMIENZO,PRIMERA_LETRA).toUpperCase() +
+        apellidos.substring(PRIMERA_LETRA,FINAL).toLowerCase() + nombre.substring(COMIENZO,FINAL).toLowerCase();
+        String apellidosStarWars = apellidoMadre.substring(COMIENZO,PRIMERA_LETRA).toUpperCase()
+        + apellidoMadre.substring(PRIMERA_LETRA,APELLIDO_FINAL).toLowerCase() +  ciudadDondeNacistes.substring(COMIENZO,FINAL).toLowerCase();
         return nombreStarWars + " " + apellidosStarWars;
     }
 }
